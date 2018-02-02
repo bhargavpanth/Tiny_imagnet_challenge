bb232fb6a01c1d6090e5c35930ae24b3ac4e5830
Bhargav Ramprasad Panth <panthb@msc-linux-sls-037.scss.tcd.ie>
2018-02-02 18:43:04 +0000

    modification in the baseline model

--- train.py
+++ train.py
@@ -96,9 +96,12 @@ def main(data_dir, hdf5, name):
 
     # Training. It will always save the best performing model on the validation data, even if it overfits.
     checkpoint_path = 'output/'+name+'/'
-    model = tflearn.DNN(network, tensorboard_verbose=0, tensorboard_dir='tensorboard', best_checkpoint_path=checkpoint_path)
-    model.fit(X, Y, n_epoch=num_epochs, shuffle=True, validation_set=(X_test, Y_test),
-    show_metric=True, batch_size=batch_size, run_id=name)
+    model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='baseline_cnn.tfl.ckpt')
+    # model = tflearn.DNN(network, tensorboard_verbose=0, tensorboard_dir='tensorboard', best_checkpoint_path=checkpoint_path)
+    
+    model.fit(X, Y, n_epoch=100, shuffle=True, validation_set=(X_test, Y_test),show_metric=True, batch_size=96,snapshot_epoch=True,run_id='baseline_cnn')
+    # model.fit(X, Y, n_epoch=num_epochs, shuffle=True, validation_set=(X_test, Y_test),
+    # show_metric=True, batch_size=batch_size, run_id=name)
 
 if __name__ == '__main__':
     # Parse arguments and create output directories.
